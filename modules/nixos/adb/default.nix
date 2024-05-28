d110b13db578125ed9a3bfa4bdf0056db82080e2
Simon W. Jackson <hello@simonwjackson.io>
2024-05-27 21:53:40 -0500

    refactor: return adb

--- modules/nixos/adb/default.nix
+++ modules/nixos/adb/default.nix
@@ -10,11 +10,17 @@
 in {
   options.backpacker.adb = {
     enable = mkEnableOption "Whether to enable adb tooling";
+
+    user = lib.mkOption {
+      type = lib.types.str;
+      default = config.backpacker.user.name;
+      description = "";
+    };
   };
 
   config = mkIf cfg.enable {
     programs.adb.enable = true;
-    users.users."${config.backpacker.user.name}".extraGroups = ["adbusers"];
+    users.users."${cfg.user}".extraGroups = ["adbusers"];
     services.udev.packages = [
       pkgs.android-udev-rules
     ];
